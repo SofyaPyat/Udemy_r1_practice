ac163a3d1a4f44afc2b22c7ecf0497513e83547c
Sofya <SofjaEsenina@yandex.ru>
2023-07-24 01:52:04 +0300

    preproc jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello, world!</h2>;
+// const elem = <h2>Hello, world!</h2>;
+// const elem = React.createElement("h2", {className: "greetings"}, "Hello, world!")   //сначала название элемента, затем название класса для элемента, затем содержимое тега
+
+const text = "Hello world";
+
+const elem = (
+  <div>
+    <h2 className="text">Текст: {text}</h2>
+    <input type = "text"/>
+    <button tabIndex="0">Click here</button> 
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
